045d03ce98fb97b4a43f46f3f5968526919569e4
Mohammed Jawad Saleem <mohammedjawadsaleem17@gmail.com>
2024-03-04 18:57:31 +0530

    Hard Work

--- src/AppDup-V1.js
+++ src/AppDup-V1.js
@@ -1,6 +1,48 @@
 import { useEffect, useState } from "react"
 import StarRating from "./StarRating"
 
+const tempMovieData = [
+    {
+        imdbID: "tt1375666",
+        Title: "Inception",
+        Year: "2010",
+        Poster: "https://m.media-amazon.com/images/M/MV5BMjAxMzY3NjcxNF5BMl5BanBnXkFtZTcwNTI5OTM0Mw@@._V1_SX300.jpg",
+    },
+    {
+        imdbID: "tt0133093",
+        Title: "The Matrix",
+        Year: "1999",
+        Poster: "https://m.media-amazon.com/images/M/MV5BNzQzOTk3OTAtNDQ0Zi00ZTVkLWI0MTEtMDllZjNkYzNjNTc4L2ltYWdlXkEyXkFqcGdeQXVyNjU0OTQ0OTY@._V1_SX300.jpg",
+    },
+    {
+        imdbID: "tt6751668",
+        Title: "Parasite",
+        Year: "2019",
+        Poster: "https://m.media-amazon.com/images/M/MV5BYWZjMjk3ZTItODQ2ZC00NTY5LWE0ZDYtZTI3MjcwN2Q5NTVkXkEyXkFqcGdeQXVyODk4OTc3MTY@._V1_SX300.jpg",
+    },
+]
+
+const tempWatchedData = [
+    {
+        imdbID: "tt1375666",
+        Title: "Inception",
+        Year: "2010",
+        Poster: "https://m.media-amazon.com/images/M/MV5BMjAxMzY3NjcxNF5BMl5BanBnXkFtZTcwNTI5OTM0Mw@@._V1_SX300.jpg",
+        runtime: 148,
+        imdbRating: 8.8,
+        userRating: 10,
+    },
+    {
+        imdbID: "tt0088763",
+        Title: "Back to the Future",
+        Year: "1985",
+        Poster: "https://m.media-amazon.com/images/M/MV5BZmU0M2Y1OGUtZjIxNi00ZjBkLTg1MjgtOWIyNThiZWIwYjRiXkEyXkFqcGdeQXVyMTQxNzMzNDI@._V1_SX300.jpg",
+        runtime: 116,
+        imdbRating: 8.5,
+        userRating: 9,
+    },
+]
+
 const average = (arr) =>
     arr.reduce((acc, cur, i, arr) => acc + cur / arr.length, 0)
 
@@ -8,17 +50,12 @@ const KEY = "65306d5d"
 
 export default function App() {
     const [movies, setMovies] = useState([])
-    // const [watched, setWatched] = useState([])
+    const [watched, setWatched] = useState([])
 
     const [isLoading, setIsLoading] = useState(false)
     const [error, setError] = useState("")
     const [query, setQuery] = useState("")
     const tempQuery = "interstellar"
-
-    const [watched, setWatched] = useState(function () {
-        const storedValue = localStorage.getItem("watched")
-        return JSON.parse(storedValue)
-    })
 
     const [selectedID, setSelectedId] = useState(null)
 
@@ -82,13 +119,6 @@ export default function App() {
     function handleCloseMovie() {
         setSelectedId(null)
     }
-
-    useEffect(
-        function () {
-            localStorage.setItem("watched", JSON.stringify(watched))
-        },
-        [watched]
-    )
 
     return (
         <>
@@ -154,7 +184,6 @@ function MovieDetails({ selectedID, onCloseMovie, onAddWatched, watched }) {
             document.removeEventListener("keydown", callback)
         }
     }, [])
-    /* eslint-disable*/
 
     const {
         Title: title,
